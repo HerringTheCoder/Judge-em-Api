c60c74f876154bd43be942253058e8cf52f53216
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-14 18:03:32 +0200

    Added Game and Item Request wrappers

---
+++ Core/Requests/ItemCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Requests
+{
+    public class ItemCreateRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageLink { get; set; }
+    }
+}
