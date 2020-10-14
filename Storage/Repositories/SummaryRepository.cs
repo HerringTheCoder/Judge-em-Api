be0ca048a0e6482789964b2c1ca9bea5825a1cff
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-14 18:01:17 +0200

    Added Game, Item and Summary repositories

---
+++ Storage/Repositories/SummaryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Storage.Repositories.Interfaces;
+using Storage.Tables;
+
+namespace Storage.Repositories
+{
+    public class SummaryRepository : BaseRepository<JudgeContext, Summary>, ISummaryRepository
+    {
+        public SummaryRepository(JudgeContext context) : base(context) { }
+    }
+
+}
