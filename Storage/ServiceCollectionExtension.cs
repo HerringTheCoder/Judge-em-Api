ff47188fd81f1246c35ad2c2c18d9d559e6d6035
Szymon Sandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-10-14 13:02:55 +0200

    Dev database schema (#9)
    * Added database models
    
    * Added database context
    
    * Added initial database migration

---
+++ Storage/ServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Storage
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddStorageLibraryServices(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<JudgeContext>(
+                options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+            return services;
+        }
+    }
+}
---
+++ Storage/Tables/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Storage.Tables
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageLink { get; set; }
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+    }
+}
