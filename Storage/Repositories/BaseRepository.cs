6deb8bb728e3de1d16fd8f8e783681d37df59975
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-14 18:00:25 +0200

    Added BaseRepository

---
+++ Storage/Repositories/BaseRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Storage.Repositories.Interfaces;
+
+namespace Storage.Repositories
+{
+    public abstract class BaseRepository<TContext, TEntity> : IBaseRepository<TEntity>
+    where TEntity : class 
+    where TContext : DbContext
+    {
+        protected TContext Context;
+
+        public BaseRepository(TContext context)
+        {
+            Context = context;
+        }
+
+        public virtual IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate).Take(1);
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public void SaveChanges()
+        {
+            try
+            {
+                Context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Storage/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace Storage.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+        void SaveChanges();
+    }
+}
