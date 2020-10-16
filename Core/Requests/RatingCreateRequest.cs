128f920f3019036614e9178e826d1438c55c0734
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-16 11:54:06 +0200

    Added Rating request wrapper

---
+++ Core/Requests/RatingCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Requests
+{
+    public class RatingCreateRequest
+    {
+        [Required]
+        public int Score { get; set; }
+        [Required]
+        public int ItemId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
