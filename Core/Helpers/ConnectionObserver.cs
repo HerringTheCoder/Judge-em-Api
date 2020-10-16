4c035567254989dc061ff70bddc9103c67173b7e
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-16 12:11:32 +0200

    Added CleanConnectionGroup method to ConnectionObserver

--- Core/Helpers/ConnectionObserver.cs
+++ Core/Helpers/ConnectionObserver.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Helpers
 {
     public static class ConnectionObserver
     {
         public static Dictionary<string, string> ConnectionStates => new Dictionary<string,string>();
+
+        public static void CleanConnectionGroup(string gameCode)
+        {
+            var keysToRemove = ConnectionStates
+                .Where(entry => entry.Value == gameCode)
+                .Select(entry => entry.Key)
+                .ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                ConnectionStates.Remove(key);
+            }
+        }
     }
 }
