d24c1cc29fe0662ed61d2a591b12aa2073af4ffa
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-16 12:12:33 +0200

    Added item->ratings filter in GameRepository

--- Storage/Repositories/GameRepository.cs
+++ Storage/Repositories/GameRepository.cs
@@ -1,4 +1,6 @@
-﻿using Storage.Repositories.Interfaces;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Storage.Repositories.Interfaces;
 using Storage.Tables;
 
 namespace Storage.Repositories
@@ -6,5 +8,12 @@ namespace Storage.Repositories
     public class GameRepository : BaseRepository<JudgeContext,Game>, IGameRepository
     {
         public GameRepository(JudgeContext context) : base (context){}
+
+        public IQueryable<Game> GetGameWithSingleItemRatings(int gameId, int itemId)
+        {
+            return Get(g => g.Id == gameId)
+                .Include(g => g.Items.FirstOrDefault(i => i.Id == itemId))
+                .ThenInclude(i => i.Ratings);
+        }
     }
 }
--- Storage/Repositories/Interfaces/IGameRepository.cs
+++ Storage/Repositories/Interfaces/IGameRepository.cs
@@ -1,6 +1,10 @@
-﻿using Storage.Tables;
+﻿using System.Linq;
+using Storage.Tables;
 
 namespace Storage.Repositories.Interfaces
 {
-    public interface IGameRepository : IBaseRepository<Game>{}
+    public interface IGameRepository : IBaseRepository<Game>
+    {
+        IQueryable<Game> GetGameWithSingleItemRatings(int gameId, int itemId);
+    }
 }
