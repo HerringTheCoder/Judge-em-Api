620ea1a5556a5b99b35b0cec55513a409eca14bc
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-16 11:54:43 +0200

    Added ConnectionObserver helper

---
+++ Core/Helpers/ConnectionObserver.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Helpers
+{
+    public static class ConnectionObserver
+    {
+        public static Dictionary<string, string> ConnectionStates => new Dictionary<string,string>();
+    }
+}
