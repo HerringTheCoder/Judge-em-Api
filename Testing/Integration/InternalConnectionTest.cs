9578c3af051d80286cc4d8e8cdad84c978d30125
Szymon Sandura <44547474+HerringTheCoder@users.noreply.github.com>
2020-10-16 10:09:43 +0200

    Add github actions running tests (#13)
    * Create dotnet-core.yml
    
    * Added testing framework and simple integration tests (#14)
    
    * Added xUnit testing project
    
    * Added simple integration test
    
    * Updated package references

---
+++ Testing/Integration/InternalConnectionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using WebApi;
+using Xunit;
+
+namespace Testing.Integration
+{
+    public class WebBuilder : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public WebBuilder(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("http://www.example.com")]
+        public async Task TestAccessToInternalAndExternalNetworks(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
