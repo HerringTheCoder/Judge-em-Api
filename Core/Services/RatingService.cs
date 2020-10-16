81103f24df56024d540c6d084da837885dc63d2c
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-16 11:52:53 +0200

    Added rating service and repository

---
+++ Core/Services/RatingService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Core.Requests;
+using Core.Services.Interfaces;
+using Storage.Repositories.Interfaces;
+using Storage.Tables;
+
+namespace Core.Services
+{
+    public class RatingService : IRatingService
+    {
+        private readonly IRatingRepository _ratingRepository;
+
+        public RatingService(IRatingRepository ratingRepository)
+        {
+            _ratingRepository = ratingRepository;
+        }
+
+        public async Task AddRating(RatingCreateRequest request, string userId)
+        {
+
+            if (int.TryParse(userId, out int id))
+            {
+                var rating = new Rating
+                {
+                    CategoryId = request.CategoryId,
+                    ItemId = request.ItemId,
+                    Score = request.Score,
+                    UserId = id
+                };
+                _ratingRepository.Add(rating);
+                await _ratingRepository.SaveChangesAsync();
+            }
+        }
+    
+    }
+}
