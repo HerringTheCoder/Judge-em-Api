07dfcf3561c24d65d58a9097a1cf94c44295ee5b
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-17 15:34:21 +0200

    Added CategoryService Unit tests

---
+++ Testing/Mocks/CategoryRepositoryMock.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Storage.Repositories.Interfaces;
+using Storage.Tables;
+
+namespace Testing.Mocks
+{
+    public class CategoryRepositoryMock : ICategoryRepository
+    {
+        public List<Category> Categories { get; set; } = new List<Category>();
+
+        public CategoryRepositoryMock(int objectsCount)
+        {
+            for (int i = 1; i <= objectsCount; i++)
+            {
+                Categories.Add(new Category
+                {
+                    Id = i,
+                    Name = $"CategoryName-{i}",
+                    Weight = i * 10
+                });
+            }
+        }
+        public IQueryable<Category> GetAll()
+        {
+            return Categories.AsQueryable();
+        }
+
+        public IQueryable<Category> Get(Expression<Func<Category, bool>> predicate)
+        {
+            return Categories.AsQueryable().Where(predicate);
+        }
+
+        public void Add(Category entity)
+        {
+            var lastObject = Categories.FindLast(c => true);
+            entity.Id = lastObject?.Id ?? 0;
+            Categories.Add(entity);
+        }
+
+        public void Delete(Category entity)
+        {
+            Categories.Remove(entity);
+        }
+
+        public void Update(Category entity)
+        {
+            var category = Categories.FirstOrDefault(c => c.Id == entity.Id);
+            category = category != null ? entity : null;
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Testing/Unit/CategoryServiceTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Core.Requests;
+using Core.Services;
+using Core.Services.Interfaces;
+using Storage.Repositories.Interfaces;
+using Storage.Tables;
+using Testing.Mocks;
+using Xunit;
+
+namespace Testing.Unit
+{
+    public class CategoryServiceTest
+    {
+        private const int ObjectsCount = 5;
+        private readonly CategoryRepositoryMock _categoryRepositoryMock;
+        private readonly ICategoryService _categoryService;
+
+        //Called before each test
+        public CategoryServiceTest()
+        {
+            _categoryRepositoryMock = new CategoryRepositoryMock(ObjectsCount);
+            _categoryService = new CategoryService(_categoryRepositoryMock);
+        }
+
+        [Fact]
+        public async Task TestIfCategoryIsCreated()
+        {
+            var categoryRequest = new CategoryCreateRequest
+            {
+                Name = "TestCategoryName",
+                Weight = 5
+            };
+            await _categoryService.CreateCategory(categoryRequest);
+            var category = _categoryRepositoryMock.Categories.Last();
+            Assert.Equal(categoryRequest.Name, category.Name);
+            Assert.Equal(categoryRequest.Weight, category.Weight);
+        }
+
+        [Fact]
+        public async Task TestIfCategoryIsDeleted()
+        {
+            await _categoryService.DeleteCategory(5);
+            Assert.Equal(ObjectsCount - 1, _categoryRepositoryMock.Categories.Count);
+        }
+
+        [Fact]
+        public void TestIfCategoriesAreRetrievable()
+        {
+            var categories = new List<Category>();
+            for (int i = 1; i <= ObjectsCount; i++)
+                categories.Add(_categoryService.GetCategory(i));
+
+            Assert.Equal(ObjectsCount, categories.Count);
+            Assert.IsType<List<Category>>(categories);
+        }
+    }
+}
