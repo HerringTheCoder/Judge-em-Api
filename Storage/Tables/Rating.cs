ddb31cdaf15e8c7ddfd0e280a0f608fde0db631a
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-17 12:16:25 +0200

    Changed Rating table TotalScore type to float

--- Storage/Tables/Rating.cs
+++ Storage/Tables/Rating.cs
@@ -7,7 +7,7 @@ namespace Storage.Tables
     public class Rating
     {
         public int Id { get; set; }
-        public int TotalScore { get; set; }
+        public float TotalScore { get; set; }
         [ForeignKey(nameof(Item))]
         public int ItemId { get; set; }
         public Item Item { get; set; }
