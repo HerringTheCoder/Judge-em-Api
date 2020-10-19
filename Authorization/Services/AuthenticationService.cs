2e6c63bdead6e3d1c04cbd159e622e72e3db5069
PR <patrykreszczynski@gmail.com>
2020-10-19 12:20:43 +0200

    Added AuthenticationService

---
+++ Authorization/Services/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using Authorization.Requests;
+using Authorization.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Authorization.Services
+{
+    class AuthenticationService : IAuthenticationService
+    {
+        public Task Login(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Register(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Authorization/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using Authorization.Requests;
+using System.Threading.Tasks;
+
+namespace Authorization.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task Register(RegisterRequest request);
+        Task Login(LoginRequest request);
+        Task Logout();
+    }
+}
