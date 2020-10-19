1044b7e46f29f95251f2648b7766e7ce4ab5705d
PR <patrykreszczynski@gmail.com>
2020-10-19 12:21:34 +0200

    Added AuthenticationController with google and facebook login

--- Authorization/ServiceCollectionExtension.cs
+++ Authorization/ServiceCollectionExtension.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Authorization
 {
     public static class ServiceCollectionExtension
     {
-        public static IServiceCollection AddAuthorizationLibraryServices(this IServiceCollection services)
+        private static IConfiguration Configuration;
+
+        public static IServiceCollection AddAuthorizationLibraryServices(this IServiceCollection services, IConfiguration configuration)
         {
-            return services;
+            Configuration = configuration;
+
+            services.AddControllers();
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JwtToken:Issuer"],
+                        ValidAudience = Configuration["JwtToken:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtToken:SecretKey"]))
+                    };
+                })
+                .AddCookie(Options =>
+                {
+                    Options.LoginPath = "/google-login";
+                    Options.LoginPath = "/facebook-login";
+                })
+                .AddGoogle(Options =>
+                {
+                    Options.ClientId = Configuration["Authentication:Google:ClientId"];
+                    Options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+
+                    Options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddFacebook(Options =>
+                {
+                    Options.AppId = Configuration["Authentication:Facebook:AppId"];
+                    Options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                });
+
+                return services;
         }
     }
 }
---
+++ WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Authorization.Requests;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [AllowAnonymous, Route("api")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Login(LoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("google-login")]
+        public IActionResult GoogleLogin()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("AuthenticationResponse")
+            };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [Route("facebook-login")]
+        public IActionResult FacebookLogin()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("AuthenticationResponse")
+            };
+
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+        }
+
+        [Route("authentication-response")]
+        public async Task<IActionResult> AuthenticationResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claims = result.Principal.Identities
+                .FirstOrDefault().Claims.Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+
+            return Ok(claims);
+        }
+    }
+}
