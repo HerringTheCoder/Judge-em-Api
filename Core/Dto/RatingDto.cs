803f6208857d986d4391aefc20b1831ece8133dd
Szymon Sandura <szymon.sandura@gmail.com>
2020-10-20 16:27:17 +0200

    Added Dto entities for response formatting

---
+++ Core/Dto/RatingDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using Storage.Tables;
+
+namespace Core.Dto
+{
+    public class RatingDto
+    {
+        public int Id { get; set; }
+        public float TotalScore { get; set; }
+        public UserDto User { get; set; }
+
+        public RatingDto(Rating rating)
+        {
+            Id = rating.Id;
+            TotalScore = rating.TotalScore;
+            User = new UserDto(rating.User);
+        }
+    }
+}
---
+++ Core/Dto/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using Storage.Tables;
+
+namespace Core.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+
+        public UserDto(User user)
+        {
+            Id = user.Id;
+            Email = user.Email;
+            Name = user.Name;
+            Nickname = user.Nickname;
+        }
+    }
+}
