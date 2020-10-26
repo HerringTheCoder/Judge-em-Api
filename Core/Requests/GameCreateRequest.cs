7f5772915e94dc9217c6f22e7eb2544e950c9b21
PR <patrykreszczynski@gmail.com>
2020-10-26 14:14:37 +0100

    Added validation rules for item, game and category request

--- Core/Requests/GameCreateRequest.cs
+++ Core/Requests/GameCreateRequest.cs
@@ -5,6 +5,7 @@ namespace Core.Requests
     public class GameCreateRequest
     {
         [Required]
+        [StringLength(30, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 1)]
         public string Name { get; set; }
     }
 }
